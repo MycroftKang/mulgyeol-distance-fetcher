e0507c8dfbf65006a28a04197708aed824abc6a3
Mycroft Kang <taet777@naver.com>
2020-06-27 23:23:31 +0900

    Fix msu runtime error

--- scripts/build.bat
+++ scripts/build.bat
@@ -4,10 +4,10 @@ python package\vc.py -lb
 cd src
 pyinstaller main.spec
 pyinstaller msu.spec
-move dist\msu\* dist\main
+xcopy /I /Y /E dist\msu\* dist\main
 move dist\main ..\build\app
 rmdir /s /q dist\msu
-xcopy ..\package\data ..\build
+xcopy /I /Y /E ..\package\data ..\build\data
 xcopy /I /Y ..\package\info ..\build\info
 copy ..\product.json ..\build
 move version_info_temp.txt version_info.txt
